e8eb5754574a90fcf25485088b0d7061fa0b2d4f
OlivierTD <oli.trep@gmail.com>
2018-02-25 12:56:51 -0500

    Created new Unit Test for pet class

---
+++ src/test/java/org/springframework/samples/petclinic/owner/TestPet.java
@@ -0,0 +1,14 @@
+package org.springframework.samples.petclinic.owner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPet {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
