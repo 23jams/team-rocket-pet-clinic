3c0ad2c64e36a41b1602c199a8845b718f083079
OlivierTD <oli.trep@gmail.com>
2018-02-25 13:18:55 -0500

    Implemented TestPet
    TestPet tests the implementation of the interface Owner within the Pet
    class.

--- src/test/java/org/springframework/samples/petclinic/owner/TestPet.java
+++ src/test/java/org/springframework/samples/petclinic/owner/TestPet.java
@@ -4,11 +4,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Test class for the Pet class
+ *
+ * @author Olivier Trépanier-Desfossés
+ */
 public class TestPet {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//Create FakeOwner Object
+		FakeOwner fakeOwner = new FakeOwner();
+		fakeOwner.setAddress("123 Street");
+		fakeOwner.setCity("Montreal");
+		fakeOwner.setTelephone("999-999-9999");
+		
+		//Create Pet Object
+		Pet pet = new Pet();
+		
+		//Assign Pet Object to FakeOwner Object
+		fakeOwner.addPet(pet); //Calls pet.setOwner(fakeOwner)
+		
+		//Test values
+		assertEquals("123 Street", pet.getOwner().getAddress());
+		assertEquals("Montreal", pet.getOwner().getCity());
+		assertEquals("999-999-9999", pet.getOwner().getTelephone());
 	}
 
 }
