bda4835f928b29af083abbc96cd51b15bbb4739b
DESKTOP-9KJ158Q\James <23jamesff@gmail.com>
2018-04-02 10:46:46 -0400

    Added functionality for scheduling and async

---
+++ src/main/java/org/springframework/samples/petclinic/ConsistencyChecker.java
@@ -0,0 +1,19 @@
+package org.springframework.samples.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class ConsistencyChecker {
+	//contains consistency checkers
+
+
+}
